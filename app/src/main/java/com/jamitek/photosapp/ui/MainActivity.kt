7d3d313538fd57734543d882e3f58d68bdb7cee2
jaakko <jaakko.jaaska@gmail.com>
2020-11-29 19:31:58 +0100

    Navigation tweaks, bumps material lib to latest

--- app/src/main/java/com/jamitek/photosapp/ui/MainActivity.kt
+++ app/src/main/java/com/jamitek/photosapp/ui/MainActivity.kt
@@ -4,24 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.NavigationUI
 import com.jamitek.photosapp.R
 import com.jamitek.photosapp.extension.dependencyRoot
 import com.jamitek.photosapp.storage.StorageAccessHelper
 import com.jamitek.photosapp.ui.viewmodel.LocalCameraViewModel
-import com.jamitek.photosapp.ui.viewmodel.BackupViewModel
 import com.jamitek.photosapp.ui.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener,
-    BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     private val navController by lazy { findNavController(R.id.navHostFragment) }
     private val viewModelFactory by lazy { ViewModelFactory(dependencyRoot) }
@@ -35,7 +32,7 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController.addOnDestinationChangedListener(this)
-        bottomNav.setOnNavigationItemSelectedListener(this)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 
     override fun onDestinationChanged(
@@ -47,17 +44,6 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         // TODO Copy nice sliding hiding animation for bottom nav bar from uptimeapp
         bottomNav.visibility =
             if (destination.id in bottomNavlessFragments) View.GONE else View.VISIBLE
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val actionId = when (item.itemId) {
-            R.id.photos -> R.id.action_global_mainFragment
-            R.id.backup -> R.id.action_global_localFoldersFragment
-            else -> R.id.action_global_mainFragment
-        }
-
-        navController.navigate(actionId)
-        return true
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
