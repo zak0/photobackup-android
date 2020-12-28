6cfc38220a910aba2c0cd685fd2410ffc8b2db11
jaakko <jaakko.jaaska@gmail.com>
2020-12-28 16:20:54 +0100

    Removes redundant androidx Observer imports

--- app/src/main/java/com/jamitek/photosapp/ui/fragment/ServerSetupFragment.kt
+++ app/src/main/java/com/jamitek/photosapp/ui/fragment/ServerSetupFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.jamitek.photosapp.R
 import com.jamitek.photosapp.databinding.FragmentServerSetupBinding
